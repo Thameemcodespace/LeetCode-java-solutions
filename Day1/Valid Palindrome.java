e557645c22b0029498eeb639d343ccd2a197b4e7
THAMEEM FARSHITHA <Thameemcodespace@gmail.com>
2026-09-21 20:54:25 +0530

    Valid Palindrome.java

---
+++ Day1/Valid Palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String a = s.toLowerCase();
+        String b = "";
+        for (int i = 0; i < a.length(); i++) {
+            char ch = a.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                b = b + ch;
+            }
+        }
+        String c = "";
+        for (int i = b.length() - 1; i >= 0; i--) {
+            c = c + b.charAt(i);
+        }
+        return b.equals(c);
+    }
+}
